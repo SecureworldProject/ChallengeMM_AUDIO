949a79acee951ca527a5951fe21520007d5dd2cf
Juanjo Guerrero <juanjoguerrero8@gmail.com>
2023-10-16 19:32:39 +0200

    Update challegenMM_audio.py

--- challegenMM_audio.py
+++ challegenMM_audio.py
@@ -10,7 +10,8 @@ from pydub import AudioSegment
 # variables globales
 # ------------------
 props_dict={} 
-DEBUG_MODE=False
+DEBUG_MODE=True
+SAMPLING_RATE = 44100
 
 def init(props):
     global props_dict
@@ -23,133 +24,158 @@ def init(props):
     # no vamos a ejecutar ahora el challenge
     return 0 # si init va mal retorna -1 else retorna 0
 
-def executeChallenge():
-    print("Starting execute")
-    folder=os.environ['SECUREMIRROR_CAPTURES']
-    print ("storage folder is :",folder)
-
-    #mecanismo de lock BEGIN
-    #-----------------------
-    lock.lockIN("AUDIO")
-
-    # pregunta si el usuario tiene movil con capacidad para grabar audio
-    # -----------------------------------------------------
-    #textos en español, aunque podrian ser parametros adicionales del challenge
-    #conexion=easygui.ynbox('¿Tienes un movil con bluetooth activo y cámara emparejado con tu PC?',"challenge MM: AUDIO", choices=("Yes","Not"))
-    conexion=messagebox.askyesno('challenge MM: AUDIO','¿Tienes un movil con bluetooth activo y emparejado a tu PC con capacidad para grabar un audio?')
-    print(conexion)
-    #Si el usuario responde que no ha emparejado móvil y PC, devolvemos clave y longitud 0
-    if (conexion==False):
-        lock.lockOUT("AUDIO")
-        print ("return key zero and long zero")
-        key=0
-        key_size=0
-        result =(key,key_size)
-        print ("result:",result)
-        return result # clave cero, longitud cero
-  
-    #popup msgbox pidiendo interaccion
-    #---------------------------------
-    #sent=easygui.ynbox(props_dict["interactionText"], "challenge MM: AUDIO", choices=("Yes","Not"))
-    sent=conexion=messagebox.askyesno('challenge MM: AUDIO',props_dict['interactionText'])
-    print(sent)
-
-    #Si el usuario responde que no ha enviado la imagen, devolvemos clave y longitud 0
-    if (sent== False):
-        lock.lockOUT("AUDIO")
-        print ("return key zero and long zero")
-        key=0
-        key_size=0
-        result =(key,key_size)
-        print ("result:",result)
-        return result # clave cero, longitud cero
+def calculateF0(filename):
+    #print("Fichero de captura", filename, " encontrado")
+    wav_data, sr = librosa.load(filename, sr= SAMPLING_RATE, mono=True)
     
-    # lectura del audio
-    #-------------------------------
-    # se supone que el usuario ha depositado un .wav usando bluetooth
-    # el nombre del audio puede ser siempre el mismo, fijado por el proxy bluetooth.
-    # aqui vamos a "forzar" el nombre del fichero para pruebas
-      
-    filename=folder+"/"+"capture.wav"
-    filename_mp3=folder+"/"+"capture.mp3"
-    filename_m4a=folder+"/"+"capture.m4a"
-    filename_aac=folder+"/"+"capture.aac"
-    converted_audio=folder+"/"+"converted_audio.wav"
+    # Calcular la STFT del audio
+    espectrograma = np.abs(librosa.stft(wav_data))
+    
+    # Encontrar el índice del pico de frecuencia más prominente en cada frame
+    indices_picos = np.argmax(espectrograma, axis=0)
+    
+    # Convertir el índice del pico en frecuencia
+    frecuencias = librosa.fft_frequencies(sr=sr)
+    
+    # Obtener las frecuencias fundamentales correspondientes a los índices de pico
+    frecuencias_fundamentales = frecuencias[indices_picos]
+    
+    # Tomar la frecuencia fundamental promedio de todos los frames
+    frecuencia_fundamental = np.mean(frecuencias_fundamentales)
 
-    if (DEBUG_MODE==True):
-        filename="test.wav"
-        filename_mp3="nokia.mp3"
-        filename_m4a="test.m4a"
-        filename_aac="test.aac"
-        converted_audio="converted_audio.wav"
-        
+    print("Frecuencia fundamental:", frecuencia_fundamental, "Hz")
+    return frecuencia_fundamental
+
+def returnKey(key):
+    if (key == 0):
+        result =(0, 0)
+    else:
+        key=round(key)
+        key_size=len(str(key))
+        result =(key, key_size)
+    print ("result:",result)
+    return result
+
+def fundamentalFrequency(filename, filename_mp3, filename_m4a, filename_aac, converted_audio):
     if os.path.exists(filename):
-        print ("Fichero de captura",filename," encontrado")
-        wav_data, sr = librosa.load(filename, sr=8000, mono=True)
-        print(wav_data.shape)
+        f0 = calculateF0(filename)
+        return f0
+    
         #mecanismo de lock END
-        #-----------------------
         lock.lockOUT("AUDIO")
 
     elif os.path.exists(filename_mp3):
         print ("Fichero de captura",filename_mp3," encontrado")
         sound = AudioSegment.from_file(filename_mp3)
         sound.export(converted_audio, format="wav")
-        wav_data, sr = librosa.load(converted_audio, sr=8000, mono=True)
-        print(wav_data.shape)
+        
+        f0 = calculateF0(converted_audio)
+        return f0
+        
         #mecanismo de lock END
-        #-----------------------
         lock.lockOUT("AUDIO")
 
     elif os.path.exists(filename_m4a):
         print ("Fichero de captura",filename_m4a," encontrado")
         sound = AudioSegment.from_file(filename_m4a)
         sound.export(converted_audio, format="wav")
-        wav_data, sr = librosa.load(converted_audio, sr=8000, mono=True)
-        print(wav_data.shape)
+        
+        f0 = calculateF0(converted_audio)
+        return f0
+
         #mecanismo de lock END
-        #-----------------------
         lock.lockOUT("AUDIO")
 
     elif os.path.exists(filename_aac):
         print ("Fichero de captura",filename_aac," encontrado")
         sound = AudioSegment.from_file(filename_aac)
         sound.export(converted_audio, format="wav")
-        wav_data, sr = librosa.load(converted_audio, sr=8000, mono=True)
-        print(wav_data.shape)
+
+        f0 = calculateF0(converted_audio)
+        return f0
+        
         #mecanismo de lock END
-        #-----------------------
         lock.lockOUT("AUDIO")
         
     else:
         print ("ERROR: el fichero de captura",filename," no existe")
-        key=0
-        key_size=0
-        result =(key,key_size)
-        print ("result:",result)
         lock.lockOUT("AUDIO")
-        return result # clave cero, longitud cero
+        return 0
+    
+def executeChallenge():
+    print("Starting execute")
+    folder=os.environ['SECUREMIRROR_CAPTURES']
+    print ("storage folder is :",folder)
 
-    if (DEBUG_MODE==False):
-        if os.path.exists(filename):    
-            os.remove(filename)
-        if os.path.exists(filename_mp3):    
-            os.remove(filename_mp3)
-        if os.path.exists(filename_m4a):    
-            os.remove(filename_m4a)
-        if os.path.exists(filename_aac):    
-            os.remove(filename_aac)
-        if os.path.exists(converted_audio):    
-            os.remove(converted_audio)
+    #mecanismo de lock BEGIN
+    lock.lockIN("AUDIO")
+
+    # lectura del audio
+    #-------------------------------
+    # se supone que el usuario ha depositado un .wav usando bluetooth
+    # el nombre del audio puede ser siempre el mismo, fijado por el proxy bluetooth.
+    # aqui vamos a "forzar" el nombre del fichero para pruebas
+      
+    """filename=folder+"\\"+"capture.wav"
+    filename_mp3=folder+"\\"+"capture.mp3"
+    filename_m4a=folder+"\\"+"capture.m4a"
+    filename_aac=folder+"\\"+"capture.aac"
+    converted_audio=folder+"\\"+"converted_audio.wav"
+    """
+
+    filename = os.path.join(folder, "capture.wav")
+    filename_mp3 = os.path.join(folder, "capture.mp3")
+    filename_m4a = os.path.join(folder, "capture.m4a")
+    filename_aac = os.path.join(folder, "capture.aac")
+    converted_audio = os.path.join(folder, "converted_audio.wav")
+
+    if (DEBUG_MODE==True):
+        filename="test.wav"
+        filename_mp3="test.mp3"
+        filename_m4a="test.m4a"
+        filename_aac="test.aac"
+        converted_audio="converted_audio.wav"
+
+        f0 = fundamentalFrequency(filename, filename_mp3, filename_m4a, filename_aac, converted_audio)
+
+    else:
+        # pregunta si el usuario tiene movil con capacidad para grabar audio
+        conexion=messagebox.askyesno('challenge MM: AUDIO','¿Tienes un movil con bluetooth activo y emparejado a tu PC con capacidad para grabar un audio?')
+        print(conexion)
         
+        #Si el usuario responde que no ha emparejado móvil y PC, devolvemos clave y longitud 0
+        if (conexion==False):
+            print ("ERROR: el móvil no se ha emparejado")
+            lock.lockOUT("AUDIO")
+            return returnKey(0)
 
-    #construccion de la respuesta
-    cad="%d"%(wav_data.shape)
-    key = bytes(cad,'utf-8')
-    key_size = len(key)
-    result =(key, key_size)
-    print ("result:",result)
-    return result
+        else:      
+            # popup msgbox pidiendo interaccion
+            sent=conexion=messagebox.askyesno('challenge MM: AUDIO',props_dict['interactionText'])
+            print(sent)
+
+            #Si el usuario responde que no ha enviado la imagen, devolvemos clave y longitud 0
+            if (sent== False):
+                print ("ERROR: la imagen no se ha enviado")
+                lock.lockOUT("AUDIO")
+                return returnKey(0)
+
+            else:
+                f0 = fundamentalFrequency(filename, filename_mp3, filename_m4a, filename_aac, converted_audio)
+            
+                if os.path.exists(filename):    
+                    os.remove(filename)
+                if os.path.exists(filename_mp3):    
+                    os.remove(filename_mp3)
+                if os.path.exists(filename_m4a):    
+                    os.remove(filename_m4a)
+                if os.path.exists(filename_aac):    
+                    os.remove(filename_aac)
+                if os.path.exists(converted_audio):    
+                    os.remove(converted_audio)
+        
+    # construccion de la respuesta
+    returnKey(f0)
 
     
 if __name__ == "__main__":
