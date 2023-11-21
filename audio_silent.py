b13aa8e439e8b2ce4d645427802ef7c9d9133fa3
IsabelHM1403 <72402308+IsabelHM1403@users.noreply.github.com>
2023-11-21 10:42:17 +0100

    Filter add

--- audio_silent.py
+++ audio_silent.py
@@ -31,52 +31,74 @@ def calculateF0(filename):
     
     # Calcular la STFT del audio
     espectrograma = np.abs(librosa.stft(wav_data))
+    #print (  espectrograma.shape);
+    #print (  espectrograma.shape[0]);
+    
+    # hay que anular las primeras 743 muestras del espectrograma para
+    # hacer un filtro paso alto que borre las f< 16000 khz
+    # porque shape es 1025,259 siendo 259 el numero de tramos de sonido con
+    # 1025 frecuencias analizadas en cada intervalo
+    # esto significa que en 1025 datos de frecuencia hay de 0 a 22050 hz
+    # por lo tanto 16000 esta en la posicion 743
+    num_frames=espectrograma.shape[1]
+    numfreqs=espectrograma.shape[0]
+    maxfrq=SAMPLING_RATE/2
+    #print ("max f=", maxfrq)
+    factor=numfreqs/maxfrq
+    fpb=int(16000*factor)
+    #print ("filtro de ",int(fpb))
+    espectrograma[0:fpb][:]=0
         
     # Encontrar el índice del pico de frecuencia más prominente en cada frame
     indices_picos = np.argmax(espectrograma, axis=0)
+    #print ("======================")
+    #print(indices_picos)
     #print("indices_picos", indices_picos)
     # Convertir el índice del pico en frecuencia
     frecuencias = librosa.fft_frequencies(sr=sr)
     #print("frecuencias", frecuencias)
     # Obtener las frecuencias fundamentales correspondientes a los índices de pico
     frecuencias_fundamentales = frecuencias[indices_picos]
-    #print("frecuencias_fundamentales", frecuencias_fundamentales)
-    # Tomar la frecuencia fundamental promedio de todos los frames
-    frecuencia_fundamental = 0
-    c = 0
-    for i in range(len(frecuencias_fundamentales)):
-        if(frecuencias_fundamentales[i]>16000):
-            m=int(frecuencias_fundamentales[i])
-            print("m es:", m)
-            if ((m>frecuencia_fundamental) & (c!=0)):
-                if((m>16000) & (m<=16500)):
-                    frecuencia_fundamental = 16
-                elif((m>16500) & (m<=17500)):
-                    frecuencia_fundamental = 17
-                elif((m>17500) & (m<=18500)):
-                    frecuencia_fundamental = 18
-                elif((m>18500) & (m<=19500)):
-                    frecuencia_fundamental = 19
-                elif((m>19500) & (m<=20500)):
-                    frecuencia_fundamental = 20
-                elif((m>20500) & (m<=21500)):
-                    frecuencia_fundamental = 21
-                elif((m>21500) & (m<=22500)):
-                    frecuencia_fundamental = 22
+    #print ("---------------------------")
+    #print(frecuencias_fundamentales);
+    frecuencia_fundamental = int(np.mean(frecuencias_fundamentales))
 
-            c+=1
-
-    print("Frecuencia fundamental:", frecuencia_fundamental, "kHz")
+    print("Frecuencia fundamental:", frecuencia_fundamental, "Hz")
     return frecuencia_fundamental
 
 def returnKey(frequency):
     if (frequency == 0):
         key = frequency
         key_size = 0
-    else:
-        cad="%d"%(frequency)
-        key = bytes(cad,'utf-8')
-        key_size = len(key)
+    elif((frequency>16000) & (frequency<=16500)):
+        frequency = 1
+    elif((frequency>16500) & (frequency<=17000)):
+        frequency = 2
+    elif((frequency>17000) & (frequency<=17500)):
+        frequency = 3
+    elif((frequency>17500) & (frequency<=18000)):
+        frequency = 4
+    elif((frequency>18000) & (frequency<=18500)):
+        frequency = 5
+    elif((frequency>18500) & (frequency<=19000)):
+        frequency = 6
+    elif((frequency>19000) & (frequency<=19500)):
+        frequency = 7
+    elif((frequency>19500) & (frequency<=20000)):
+        frequency = 8
+    elif((frequency>20000) & (frequency<=20500)):
+        frequency = 9
+    elif((frequency>20500) & (frequency<=21000)):
+        frequency = 10
+    elif((frequency>21000) & (frequency<=21500)):
+        frequency = 11
+    elif((frequency>21500) & (frequency<=22000)):
+        frequency = 12
+
+
+    cad="%d"%(frequency)
+    key = bytes(cad,'utf-8')
+    key_size = len(key)
     result = (key, key_size)
     print ("result:",result)
     return result
@@ -93,7 +115,7 @@ def executeChallenge():
     CHANNELS = 1
     RATE = 44100
     CHUNK = 512
-    RECORD_SECONDS = 3
+    RECORD_SECONDS = 4
     device_index = 2
     audio = pyaudio.PyAudio()
     
@@ -142,7 +164,7 @@ def executeChallenge():
     filename = os.path.join(dataPath, "capture.wav")
 
     f0 = calculateF0(filename)
-    os.remove(filename)
+    #os.remove(filename)
     lock.lockOUT("Audio_Silent")
         
     # construccion de la respuesta
